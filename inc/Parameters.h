411ae1c98ac9df694814852d4565797468dbf0b2
gracz21 <kamil.walek21@gmail.com>
2015-05-26 19:38:02 +0200

    Improved arguments parsing

--- inc/Parameters.h
+++ inc/Parameters.h
@@ -6,6 +6,8 @@ struct Parameters
 	int m; //num of bums
 	int p; //num of doctors
 	int s; //num of places for bums
+	bool report; //decides that program writes on std informations (about entering critical section,
+				//etc...) or not
 };
 
 #endif
--- src/ArgParsing.cpp
+++ src/ArgParsing.cpp
@@ -2,23 +2,34 @@
 #include <cstdlib> 
 
 int parse(Parameters &param, int argc, char * const argv[]) {
-	int opt;
-	if(argc > 4) {
-		while((opt = getopt (argc, argv, "m:p:s:")) != -1) {
-			switch(opt) {
-				case 'm':
-					param.m = atoi(optarg);
-				break;
-				case 'p':
-					param.p = atoi(optarg);
-				break;
-				case 's':
-					param.s = atoi(optarg);
-				break;
-				default:
-				break;
-			}
+	int opt, count = 0;
+
+	param.report = true;
+
+	while((opt = getopt (argc, argv, "m:p:s:v")) != -1) {
+		switch(opt) {
+			case 'm':
+				param.m = atoi(optarg);
+				count++;
+			break;
+
+			case 'p':
+				param.p = atoi(optarg);
+				count++;
+			break;
+
+			case 's':
+				param.s = atoi(optarg);
+				count++;
+			break;
+			
+			case 'v':
+				param.report = false;
+			break;
 		}
+	}
+
+	if(count >= 3)
 		return 0;
-	} else return -1;
+	else return -1;
 }
