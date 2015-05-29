8840d71c1bc6a7158794bb77a9f647013047213f
gracz21 <kamil.walek21@gmail.com>
2015-05-29 16:51:56 +0200

    Added funcionality to init program

--- inc/Init.h
+++ inc/Init.h
@@ -4,17 +4,8 @@
 #include <unistd.h>
 #include "Parameters.h"
 
-/*
-* Parse arguments from console.
-* Return 0 if 2 expected parameters (p, s) was given, -1 otherwise.
-*/
-int parse(Parameters &param, int argc, char * const argv[]);
-/*
-* Check parameters correctness and initialize objects (n - 1 process create bum object,
-* first process create museum service object)
-* Return 0 if parameters was correct and objects was initialized. 
-* Return negativ value (-1 or -2) if parameters wasn't correct.
-*/
-int initialize(int rank, int size);
+int parseParameters(Parameters &param, int argc, char * const argv[], int size);
+int randWeight(int rank);
+int* createBumsIds(int lastBumId);
 
 #endif
